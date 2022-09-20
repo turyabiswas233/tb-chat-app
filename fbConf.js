7a198671a89432482ffcea0b119bf3c66c351337
Turya Biswas <78296735+turyabiswas233@users.noreply.github.com>
2022-09-20 12:35:15 +0600

    remove log

--- fbConf.js
+++ fbConf.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-console.log(process.env.API_KEY);
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
